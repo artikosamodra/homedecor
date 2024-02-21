cc8f8632c4f589eb0e984ac16f39034fabdb637a
arthur14 <artikosamudra@gmail.com>
2024-02-21 00:32:00 -0800

    add NavBar

---
+++ src/app/component/Navbar/NavMenu.jsx
@@ -0,0 +1,11 @@
+import Link from "next/link"
+
+const NavMenu = ( {navTitle, navLink} ) => {
+    return(
+        <div>
+            <Link href={navLink} className="hover:text-zinc-600 font-bold border-b hover:border-red-500 transition duration-700 border-b-2 border-transparent py-2">{navTitle}</Link>
+        </div>
+    )
+}
+
+export default NavMenu
--- src/app/component/Navbar/Navbar.jsx
+++ src/app/component/Navbar/Navbar.jsx
@@ -1,8 +1,28 @@
+import Link from "next/link"
+import NavMenu from "./NavMenu"
+import Image from "next/image"
 
 const Navbar = () => {
     return (
-        <section>
-            <div>A</div>
+        <section className="px-20 py-5 bg-rose-100">
+            <div className="flex justify-between items-center">
+                <div>
+                    <Link href="/" >
+                        <Image src="/logo.png" alt="Logo" width={100} height={100}/>
+                    </Link>
+                </div>
+                <div className="flex flex-row gap-5">
+                    <NavMenu navTitle="HOME" navLink="/" />
+                    <NavMenu navTitle="WHO WE ARE" navLink="/" />
+                    <NavMenu navTitle="WHAT WE OFFER" navLink="/" />
+                    <NavMenu navTitle="OUR CLIENT" navLink="/" />
+                    <NavMenu navTitle="OUR TEAM" navLink="/" />
+                    <NavMenu navTitle="CONTACT" navLink="/" />
+                </div>
+                <div>
+                    <Link href="/" className="font-bold hover:text-red-500">081222333444</Link>
+                </div>
+            </div>
         </section>
     )
 }
