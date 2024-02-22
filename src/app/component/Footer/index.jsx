130020c55ee72263f16f900727b95f13993b690b
arthur14 <artikosamudra@gmail.com>
2024-02-22 09:57:13 -0800

    Add Footer

---
+++ src/app/component/Footer/index.jsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+    return(
+        <section className="pb-3 bg-zinc-900">
+            <div className="bg-red-700 px-20 py-4">
+                <p className="text-white"> &copy; 2024 Homedecor Solution. || All rights reserved.</p>
+            </div>
+        </section>
+    )
+}
+
+export default Footer
