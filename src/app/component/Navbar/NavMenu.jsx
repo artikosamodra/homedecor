e9f77bd178e3ed676b3ce087e77e084cdb047331
arthur14 <artikosamudra@gmail.com>
2024-02-22 23:20:22 -0800

    add responsive Navbar

--- src/app/component/Navbar/NavMenu.jsx
+++ src/app/component/Navbar/NavMenu.jsx
@@ -2,7 +2,7 @@ import Link from "next/link"
 
 const NavMenu = ( {navTitle, navLink} ) => {
     return(
-        <div>
+        <div className="text-center">
             <Link href={navLink} className="hover:text-zinc-600 font-bold border-b hover:border-red-500 transition duration-700 border-b-2 border-transparent py-2">{navTitle}</Link>
         </div>
     )
--- src/app/component/Navbar/Navbar.jsx
+++ src/app/component/Navbar/Navbar.jsx
@@ -1,26 +1,43 @@
+"use client"
 import Link from "next/link"
 import NavMenu from "./NavMenu"
 import Image from "next/image"
+import { List } from "@phosphor-icons/react/dist/ssr"
 
 const Navbar = () => {
+    const toggleMenu = () => {
+        const navList = document.getElementById("navlist");
+    
+        if (navList.classList.contains("hidden")) {
+          navList.classList.remove("hidden");
+        } else {
+          navList.classList.add("hidden");
+        }
+      };
+    // iconNav = document.querySelector('#iconNav');
+    // menuBar = document.querySelector("#navbar");
+
+
     return (
-        <section className="px-20 py-5 bg-rose-100 fixed top-0 w-full z-10">
-            <div className="flex justify-between items-center">
-                <div>
+        <section className="lg:px-20 px-5 py-5 bg-rose-100 fixed top-0 w-full z-10">
+            <div className="flex justify-between lg:flex-row flex-col items-center lg:text-md text-sm" id="navbar">
+                <div className="flex justify-between lg:w-20 w-full">
                     <Link href="/" >
-                        <Image src="/logo.png" alt="Logo" width={100} height={100}/>
+                        <Image src="/logo.png" alt="Logo" width={100} height={100} />
                     </Link>
+                    <button className="lg:hidden" onClick={toggleMenu}>
+                    <List size={32} />
+                </button>
                 </div>
-                <div className="flex flex-row gap-5">
-                    <NavMenu navTitle="HOME" navLink="/" />
-                    <NavMenu navTitle="WHO WE ARE" navLink="/" />
-                    <NavMenu navTitle="WHAT WE OFFER" navLink="/" />
-                    <NavMenu navTitle="OUR CLIENT" navLink="/" />
-                    <NavMenu navTitle="OUR TEAM" navLink="/" />
-                    <NavMenu navTitle="CONTACT" navLink="/" />
-                </div>
-                <div>
-                    <Link href="/" className="font-bold hover:text-red-500">081222333444</Link>
+                <div className="lg:flex hidden lg:flex-row flex-col justify-center gap-10 lg:mt-0 mt-10" id="navlist">
+                    <div className="flex flex-row gap-5 lg:flex-row flex-col">
+                        <NavMenu navTitle="HOME" navLink="/" />
+                        <NavMenu navTitle="WHO WE ARE" navLink="/" />
+                        <NavMenu navTitle="WHAT WE OFFER" navLink="/" />
+                        <NavMenu navTitle="OUR CLIENT" navLink="/" />
+                        <NavMenu navTitle="OUR TEAM" navLink="/" />
+                        <NavMenu navTitle="CONTACT" navLink="/" />
+                    </div>
                 </div>
             </div>
         </section>
