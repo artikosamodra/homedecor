d03762db8647f65f52141b203515a7178eacb104
artikosamodra <artikosamudra@gmail.com>
2024-02-25 11:28:41 +0700

    responsive info section

--- src/app/page/Info/index.jsx
+++ src/app/page/Info/index.jsx
@@ -1,20 +1,39 @@
-import Image from "next/image"
+import Image from "next/image";
 
 const InfoSec = () => {
-    return(
-        <section>
-            <div className="grid grid-cols-4 gap-10 p-20">
-                <div className="col-span-1">
-                    <Image src="/infoimg.jpg" alt="Image Info" width={400} height={400} />
-                </div>
-                <div className="col-span-3">
-                    <h1 className="text-4xl font-bold text-red-600 pb-10">Who We Are</h1>
-                    <h2 className="text-2xl font-bold  pb-10">Sub Who We Are</h2>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident qui ducimus repudiandae veniam expedita vitae possimus, quam necessitatibus saepe suscipit.</p>
-                </div>
-            </div>
-        </section>
-    )
-}
+  return (
+    <section>
+      <div className="grid grid-cols-4 gap-10 md:p-20 p-5">
+        <div className="md:col-span-1 col-span-4">
+          <Image
+            src="/infoimg.jpg"
+            alt="Image Info"
+            width={400}
+            height={400}
+            className="md:block hidden"
+          />
+        </div>
+        <div className="md:col-span-3 col-span-4">
+          <h1 className="text-4xl font-bold text-red-600 pb-8">Who We Are</h1>
+          <h2 className="text-2xl font-bold  pb-4">Sub Who We Are</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident
+            qui ducimus repudiandae veniam expedita vitae possimus, quam
+            necessitatibus saepe suscipit.
+          </p>
+        </div>
+        <div className="md:col-span-1 col-span-4">
+          <Image
+            src="/infoimg.jpg"
+            alt="Image Info"
+            width={400}
+            height={400}
+            className="h-1/2 object-cover md:hidden"
+          />
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default InfoSec
+export default InfoSec;
