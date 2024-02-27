00c79d2cf1a5e9b3355492a2910261ea0a0866e2
artikosamodra <artikosamudra@gmail.com>
2024-02-28 00:18:30 +0700

    Add Responsinve Services Sec

--- src/app/page/Hero/index.jsx
+++ src/app/page/Hero/index.jsx
@@ -1,17 +1,15 @@
 const HeroSec = () => {
   return (
     <section>
-      <div className="">
-        <div className="bg-hero relative w-full h-screen bg-cover bg-center flex flex-col items-center justify-center text-center gap-10 text-zinc-200">
-          <h1 className="text-6xl font-bold">TITLE HERO</h1>
-          <p className="text-center font-bold">
-            Descript : Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-            Porro officiis expedita voluptatem illum, cumque dignissimos!
-          </p>
-          <button className="px-5 py-2 bg-red-600 rounded-md font-bold hover:bg-red-500">
-            CONTACT US
-          </button>
-        </div>
+      <div className="bg-hero relative w-full h-screen bg-cover bg-center flex flex-col items-center justify-center text-center gap-10 text-zinc-200 md:px-20 px-8">
+        <h1 className="text-6xl font-bold">TITLE HERO</h1>
+        <p className="text-center font-bold">
+          Descript : Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+          Porro officiis expedita voluptatem illum, cumque dignissimos!
+        </p>
+        <button className="px-5 py-2 bg-red-600 rounded-md font-bold hover:bg-red-500">
+          CONTACT US
+        </button>
       </div>
     </section>
   );
--- src/app/page/Services/index.jsx
+++ src/app/page/Services/index.jsx
@@ -3,18 +3,18 @@ import Image from "next/image";
 const ServicesSec = () => {
   return (
     <section className="bg-neutral-400">
-      <div className="md:p-20 p-0 text-white font-bold">
+      <div className="md:px-20 px-8 py-20 text-white font-bold">
         <h1 className="pb-10 text-4xl text-red-700">What We Offer</h1>
-        <div className="flex flex-row justify-between items-center">
-          <div className="bg-services bg-name1 flex justify-center items-center">
+        <div className="flex md:flex-row flex-col justify-between items-center gap-5 mt-10">
+          <div className="lg:w-64 w-48 lg:h-64 h-48 bg-cover bg-center bg-name1 flex justify-center items-center">
             <h1 className="text-center text-xl p-5">
               Lorem ipsum dolor sit amet.
             </h1>
           </div>
-          <div className="bg-services bg-name2 flex justify-center items-center">
+          <div className="lg:w-64 w-48 lg:h-64 h-48 bg-cover bg-center bg-name2 flex justify-center items-center">
             <h1 className="text-center text-xl p-5">Lorem ipsum dolor.</h1>
           </div>
-          <div className="bg-services bg-name3 flex justify-center items-center">
+          <div className="lg:w-64 w-48 lg:h-64 h-48 bg-cover bg-center bg-name3 flex justify-center items-center">
             <h1 className="text-center text-xl p-5">Lorem ipsum.</h1>
           </div>
         </div>
