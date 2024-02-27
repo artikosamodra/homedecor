1ca5e312d3e9dd8333fc290a23808fdb78e4b22f
artikosamodra <artikosamudra@gmail.com>
2024-02-28 02:22:04 +0700

    gap setting services page

--- src/app/page/Services/index.jsx
+++ src/app/page/Services/index.jsx
@@ -5,7 +5,7 @@ const ServicesSec = () => {
     <section className="bg-neutral-400 pt-10" id="services">
       <div className="lg:px-20 px-8 py-20 text-white font-bold">
         <h1 className="pb-10 text-4xl text-red-700">What We Offer</h1>
-        <div className="flex md:flex-row flex-col justify-between items-center gap-10 mt-10">
+        <div className="flex md:flex-row flex-col justify-center items-center gap-14 mt-10">
           <div className="lg:w-64 w-48 lg:h-64 h-48 bg-cover bg-center bg-name1 flex justify-center items-center">
             <h1 className="text-center md:text-xl text-md p-5">
               Beautiful, Affrodable Homes for Professionals.
