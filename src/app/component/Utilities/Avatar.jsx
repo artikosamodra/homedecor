4272fea9253fe1ddf65fc9a54a0b5feefe667821
artikosamodra <artikosamudra@gmail.com>
2024-02-28 00:33:49 +0700

    Add Responsive Team Sec

--- src/app/component/Utilities/Avatar.jsx
+++ src/app/component/Utilities/Avatar.jsx
@@ -1,11 +1,17 @@
-import Image from "next/image"
+import Image from "next/image";
 
-const Avatar = ( {ImgProfil} ) => {
-    return(
-        <div>
-            <Image src={ImgProfil} alt="Logo 1" width={200} height={200} className="rounded-full ava-team" />
-        </div>
-    )
-}
+const Avatar = ({ ImgProfil }) => {
+  return (
+    <div>
+      <Image
+        src={ImgProfil}
+        alt="Logo 1"
+        width={200}
+        height={200}
+        className="rounded-full ava-team lg:w-32 w-24 lg:h-32 h-24"
+      />
+    </div>
+  );
+};
 
-export default Avatar
+export default Avatar;
--- src/app/page/Team/index.jsx
+++ src/app/page/Team/index.jsx
@@ -1,20 +1,27 @@
-import Avatar from "@/app/component/Utilities/Avatar"
+import Avatar from "@/app/component/Utilities/Avatar";
 
 const TeamSec = () => {
-    return(
-        <section>
-        <div className="bg-team flex flex-col justify-center items-center text-white gap-20">
-            <h1 className="text-4xl font-bold">OUR TEAM</h1>
-            <p className="text-center font-bold px-20">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur, doloremque, sint quia aliquam corporis corrupti voluptatem suscipit adipisci, similique eaque soluta? Neque saepe modi magnam a tempore vel, officiis exercitationem reprehenderit rem nihil, consectetur delectus? Ullam a adipisci ea reiciendis nisi harum voluptatum tempora fugit voluptates nihil? Praesentium, vero eum.</p>
-            <div className="flex flex-wrap justify-center gap-10">
-                    <Avatar ImgProfil="/infoimg.jpg" />
-                    <Avatar ImgProfil="/services1.jpg" />
-                    <Avatar ImgProfil="/services2.jpg" />
-                    <Avatar ImgProfil="/services3.jpg" />
-                </div>
+  return (
+    <section>
+      <div className="bg-team relative w-full h-full bg-cover bg-center flex flex-col items-center justify-center text-center gap-10 text-zinc-200 md:px-20 px-8 py-20">
+        <h1 className="text-4xl font-bold">OUR TEAM</h1>
+        <p className="text-center font-bold px-20">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur,
+          doloremque, sint quia aliquam corporis corrupti voluptatem suscipit
+          adipisci, similique eaque soluta? Neque saepe modi magnam a tempore
+          vel, officiis exercitationem reprehenderit rem nihil, consectetur
+          delectus? Ullam a adipisci ea reiciendis nisi harum voluptatum tempora
+          fugit voluptates nihil? Praesentium, vero eum.
+        </p>
+        <div className="flex flex-wrap justify-center lg:gap-20 gap-5">
+          <Avatar ImgProfil="/infoimg.jpg" />
+          <Avatar ImgProfil="/services1.jpg" />
+          <Avatar ImgProfil="/services2.jpg" />
+          <Avatar ImgProfil="/services3.jpg" />
         </div>
+      </div>
     </section>
-    )
-}
+  );
+};
 
-export default TeamSec
+export default TeamSec;
